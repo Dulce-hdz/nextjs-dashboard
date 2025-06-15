3b6afd1a6c82373cc55abf646813c32454ac657c
Dulce-hdz <ducehzd44@gmail.com>
2025-06-15 17:24:01 -0600

    Update YoutubeIframe.tsx

--- app/ui/iFrames/YoutubeIframe.tsx
+++ app/ui/iFrames/YoutubeIframe.tsx
@@ -1,16 +1,15 @@
 export default function YoutubeIframe() {
   return (
-    <div className="rounded-lg shadow-lg p-4 bg-white">
+    <div className="shadow rounded-lg p-4 bg-white">
       <h2 className="text-lg font-semibold mb-2">Video de YouTube</h2>
       <div className="aspect-w-16 aspect-h-9">
         <iframe
           className="w-full h-[315px] rounded"
           src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-          title="YouTube video"
+          title="Video de YouTube"
           allowFullScreen
         ></iframe>
       </div>
     </div>
   );
 }
-
